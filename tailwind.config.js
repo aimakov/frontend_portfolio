e406807771b1865f546b4fd84044409adf064476
aimakov <nurbolat.aimakov@gmail.com>
2022-09-17 19:32:01 -1000

    Working on navbar and Intro page

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx}"],
+    theme: {
+        extend: {
+            screens: {
+                sm: "480px",
+            },
+        },
+    },
+    plugins: [],
+};
